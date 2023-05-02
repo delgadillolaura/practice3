c57f9e226b37959447298644a89df8b1feeade4f
Laura Delgadillo <delgadilloarterolaura@gmail.com>
2023-05-01 21:12:10 -0400

    P3-001 Added log message to include the name of the environment that the application is running in.

--- PracticeNet/Program.cs
+++ PracticeNet/Program.cs
@@ -36,6 +36,9 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+// Log application environment
+Log.Information("Application running in {Environment} environment", builder.Environment.EnvironmentName);
+
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "QA" || app.Environment.EnvironmentName == "UAT")
 {
