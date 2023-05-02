29d33f3006239542b8cb80a7a7960d32b744e5ac
Laura Delgadillo <delgadilloarterolaura@gmail.com>
2023-05-01 22:26:05 -0400

    P3-001 Added Get, GetByCi, Put, Post and Delete endpoints. Added Http and Route attributes.

--- PracticeNet/Controllers/PatientController.cs
+++ PracticeNet/Controllers/PatientController.cs
@@ -10,4 +10,37 @@ public class PatientController : ControllerBase
     public PatientController()
     {
     }
+
+    [HttpGet]
+    public List<Patient> Get()
+    {
+        return new List<Patient>();
+    }
+
+    [HttpGet]
+    [Route("{ci}")]
+    public Patient GetByCi([FromRoute] int ci)
+    {
+        return new Patient();
+    }
+
+    [HttpPut]
+    [Route("{ci}")]
+    public Patient Put([FromRoute] int ci)
+    {
+        return new Patient();
+    }
+
+    [HttpPost]
+    public Patient Post()
+    {
+        return new Patient();
+    }
+
+    [HttpDelete]
+    [Route("{ci}")]
+    public Patient Delete([FromRoute] int ci)
+    {
+        return new Patient();
+    }
 }
