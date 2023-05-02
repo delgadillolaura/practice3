f5837c9301e85df63edce7cb00dd69f745072b87
Laura Delgadillo <delgadilloarterolaura@gmail.com>
2023-05-01 22:11:44 -0400

    P3-001 Renamed WeatherForecast.cs to Patient.cs and moved it to the ‘Models’ folder. Configured properties and changed namespace of Patient.cs file. Renamed WeatherForecastController.cs to PatientController.cs. Configured and changed namespace of PatientController.cs file.

---
+++ PracticeNet/Controllers/PatientController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using UPB.PracticeNet.Models;
+
+namespace UPB.PracticeNet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PatientController : ControllerBase
+{
+    public PatientController()
+    {
+    }
+}
